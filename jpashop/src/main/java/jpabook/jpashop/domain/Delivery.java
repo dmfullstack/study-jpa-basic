4164abc22f3673e499f3b31c953a62898aa20a90
eezy <dev.eezy0@gmail.com>
2019-11-28 21:25:56 +0900

    relation mapping

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+}
