b850884da469370fa3006b190ef3f770ecf18ae8
eezy <dev.eezy0@gmail.com>
2019-12-05 20:49:22 +0900

    paging

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,8 @@
 package jpql;
 
+import jpql.domain.Address;
 import jpql.domain.Member;
+import jpql.dto.MemberDTO;
 
 import javax.persistence.*;
 import java.util.List;
@@ -13,16 +15,46 @@ public class JpaMain {
 
         tx.begin();
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
 
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
+
+
+
+            em.flush();
+            em.clear();
+
+            // 엔티티 타입 프로젝션
             Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
                     .setParameter("username", "member1")
                     .getSingleResult();
 
             System.out.println(findMember.getUsername());
+
+
+            // 임베디드 타입 프로젝션
+            em.createQuery("select o.address from Order o", Address.class)
+                    .getResultList();
+
+            // 스칼라 타입 프로젝션
+            List<MemberDTO> resultList = em.createQuery("select distinct new jpql.dto.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
+
+            // 페이징
+            List<Member> resultList1 = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : resultList1) {
+                System.out.println(member);
+            }
+
+
+
 
             tx.commit();
         } catch (Exception e) {
