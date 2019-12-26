c9fd015a3b79eb3237f2e689631fc4ffbe899b7f
eezy <dev.eezy0@gmail.com>
2019-12-26 20:15:44 +0900

    member test

---
+++ spring-boot-jpa/src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,56 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("eezy");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertThat(member).isEqualTo(memberRepository.findOne(savedId));
+
+    }
+    
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("eezy");
+
+        Member member2 = new Member();
+        member2.setName("eezy");
+        
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        //then
+        fail("예외가 발생해야 한다.");
+    }
+
+}
