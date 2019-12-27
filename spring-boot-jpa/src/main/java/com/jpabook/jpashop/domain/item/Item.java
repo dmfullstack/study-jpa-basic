8a3af5149341f4ab444abadc34408f12ad1cfd5d
eezy <dev.eezy0@gmail.com>
2019-12-27 22:44:15 +0900

    edit item

--- spring-boot-jpa/src/main/java/com/jpabook/jpashop/domain/item/Item.java
+++ spring-boot-jpa/src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jpabook.jpashop.domain.item;
 
 import com.jpabook.jpashop.domain.Category;
+import com.jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+    }
 }
