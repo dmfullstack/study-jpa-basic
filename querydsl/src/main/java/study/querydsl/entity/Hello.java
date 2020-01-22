690bc0f9872ae0626263d9a61d88ec571c115ed4
eezy <dev.eezy0@gmail.com>
2020-01-22 23:15:51 +0900

    start query dsl

---
+++ querydsl/src/main/java/study/querydsl/entity/Hello.java
@@ -0,0 +1,16 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class Hello {
+    @Id @GeneratedValue
+    private Long id;
+}
